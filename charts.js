26c334136eed84ef661075ce562e1979b72e6103
KMontero606 <kayemontero@gmail.com>
2022-09-01 15:49:26 -0700

    Added bubble chart

--- charts.js
+++ charts.js
@@ -85,9 +85,49 @@ function buildCharts(sample) {
     };
     // 9. Create the layout for the bar chart. 
     var layout = {
-      title: "Top 10 bacterial species (OTUs)"
+      title: "Top 10 bacterial species (OTUs)",
+      autosize: true
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", trace, layout);
   });
 }
+
+// Bar and Bubble charts
+// Create the buildCharts function.
+function buildCharts(sample) {
+  // Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    
+
+    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", trace, layout); 
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sampleValues,
+        color: otuIDs
+        //colorscale: 
+      }
+   }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: {title: 'OTU ID', automargin: true},
+      yaxis: {automargin: true},
+      // margins: 
+      hovermode: "closest"
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+  });
+}
+
